219a5fba021270b325d04752571896078b402396
Zhongliu Han <Zhongliu.Han@edu.sait.ca>
2018-03-18 11:08:04 -0600

    added hashPassword method to UserDB.java

--- src/java/sait/db/UsersDB.java
+++ src/java/sait/db/UsersDB.java
@@ -5,6 +5,8 @@
  */
 package sait.db;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -140,5 +142,23 @@ public class UsersDB
             return false;
         }
 
+    }
+
+    public static String hashPassword(String password) throws NoSuchAlgorithmException
+    {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes());
+        byte[] mdArray = md.digest();
+        StringBuilder sb = new StringBuilder(mdArray.length*2);
+        for(byte b: mdArray)
+        {
+            int v = b & 0xff;
+            if(v < 16)
+            {
+                sb.append('0');
+            }
+            sb.append(Integer.toHexString(v));
+        }
+        return sb.toString();
     }
 }
